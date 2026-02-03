ea44181041b14b790577d96c0274bcd5e1d2463b
mike3210976 <142404246+mike3210976@users.noreply.github.com>
2026-02-03 15:00:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-
 // vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // ⬅️ usklajeno s package.json
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // NUJNO za custom domeno na rootu (sacredescapes.eu)
+  base: '/', // pravilno za custom domeno na rootu (https://sacredescapes.eu)
 })
